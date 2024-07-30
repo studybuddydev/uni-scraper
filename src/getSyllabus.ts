8f8e50bdb73d6cef0199095599bc125ccabce44a
alessiogandelli <alessiogandelli99@gmail.com>
2024-07-30 14:09:34 +0200

    modularize

--- src/getSyllabus.ts
+++ src/getSyllabus.ts
@@ -4,47 +4,27 @@ import 'dotenv/config'
 import fs from 'fs';
 const openai = new OpenAI();
 
-
 export interface CourseInfo {
     name: string;
     icon: string;
     color: string;
     chapters: string;
-    links:string[]
-    postIts:string[]
+    links: string[]
+    postIts: string[]
     books: string;
     examDetails: string;
     learningGoals: string;
     methods: string;
 }
 
+interface processedChapter {
+    icon: string,
+    color: string,
+    chapters: []
+}
 
-
-
-
-// get url of a syllabus from unitn course catalogue and scrape it 
-export async function scrapeSyllabus(url: string): Promise<CourseInfo> {
-    const browser = await puppeteer.launch();
-    const page: Page = await browser.newPage();
-    //const url = ctx.message?.text as string
-    await page.goto(url, { waitUntil: 'networkidle0' }); // Replace with your target web app URL
-
-    await page.waitForSelector('app-root', { timeout: 5000 });
-
-
-    await page.waitForSelector('.u-filetto');
-
-    // Extract the title text
-    const title = await page.$eval('.u-filetto', element => element.textContent);
-
-
-    console.log(`Title: ${title}`);
-
-    await page.waitForSelector('.accordion');
-    let infoElements: CourseInfo = { name: '',icon:'', color:'', chapters: '',links:[], postIts: [], books: '', examDetails: '', learningGoals: '', methods: '' };
-
-    // unico modo di farlo funzionare, ho provato a tirare fuori sta funzione per pulire un po' il codice ma non funziona
-    infoElements = await page.$$eval('.accordion > dt, .accordion > dd', (elements: Element[]) => {
+async function getCourseInfo(page: Page): Promise<CourseInfo> {
+    return await page.$$eval('.accordion > dt, .accordion > dd', (elements: Element[]) => {
         let currentGroup: Partial<CourseInfo> = {};
 
         for (const element of elements) {
@@ -78,34 +58,57 @@ export async function scrapeSyllabus(url: string): Promise<CourseInfo> {
 
         return currentGroup as CourseInfo;
     });
-
-     infoElements.name = title?.trim() || 'no title found';
-
+}
 
 
 
-    //save to a json file 
+// get url of a syllabus from unitn course catalogue and scrape it 
+export async function scrapeSyllabus(url: string): Promise<CourseInfo> {
+    const browser = await puppeteer.launch();
+    const page: Page = await browser.newPage();
+    //const url = ctx.message?.text as string
+    await page.goto(url, { waitUntil: 'networkidle0' }); // Replace with your target web app URL
 
+    await page.waitForSelector('app-root', { timeout: 5000 });
+
+    await page.waitForSelector('.u-filetto');
+
+    // Extract the title text
+    const title = await page.$eval('.u-filetto', element => element.textContent);
+
+
+    console.log(`Title: ${title}`);
+
+    await page.waitForSelector('.accordion');
+    let infoElements: CourseInfo = { name: '', icon: '', color: '', chapters: '', links: [], postIts: [], books: '', examDetails: '', learningGoals: '', methods: '' };
+
+    // unico modo di farlo funzionare, ho provato a tirare fuori sta funzione per pulire un po' il codice ma non funziona
+
+    try{
+        infoElements = await getCourseInfo(page)
+    }catch(e){
+        console.log('scassato');
+        
+    }
     
+
+    infoElements.name = title?.trim() || 'no title found';
 
 
     await browser.close();
 
-    //const processedSyllabus = await processSyllabus(infoElements)
-    //const inputfile: InputFile = new InputFile(Buffer.from(JSON.stringify(processedSyllabus, null, 2)), processedSyllabus?.name + '.json')
 
-
-    return  infoElements
+    return infoElements
 
 
 
 }
 
 
-async function processSyllabusChapters(syllabus: CourseInfo) {
+async function processSyllabusChapters(syllabus: CourseInfo): Promise<processedChapter> {
     const systemPromptChapters = `You are a helpful studybuddy for university students, you are given in input a string with the content of the course, 
-                        you should infer the chapters and the section(if present), add notes in the postIts if needed to clarify, create a json 
-                        output should follow this schema:{chapters: [{ name: string, sections: string[], postIts: {"color": "#e6b905","content": ""}, links:string[]}`
+                        you should infer the chapters and the section(if present) and put it in the tasks list with done set to false, add notes in the postIts if needed to clarify, use a representative mdi icon provising the its name and a random hex color, create a json 
+                        output should follow this schema:{ icon: string, color:string, chapters: [{ name: string, showTasks: true, tasks: {name: string, done: bool}[], postIts: {"color": "#e6b905","content": ""}[], links:string[]}`
 
 
 
@@ -124,46 +127,63 @@ async function processSyllabusChapters(syllabus: CourseInfo) {
     return parsedChapters
 
 
-    // const systemPromptBooks = `You are a helpful studybuddy for university students, you are given in input a string with the content of the course, 
-    //   you should infer list of the books used in this universitycourse, create a json a and fill the schema as you believe it is useful for the student, the 
-    //   output should follow this schema:{books: [{ name: string, authors: string[], notes: string[]}`
-
-    // response = await openai.chat.completions.create({
-    //     model: "gpt-4o-mini",  // Make sure to specify the correct model
-    //     messages: [
-    //         { role: "system", content: systemPromptBooks },
-    //         { role: "user", content: syllabus.books },
-    //     ],
-    //     response_format: { type: "json_object" },
-    // });
-
-    // const parsedBooks = response.choices[0].message.content
-
-    // return({title:syllabus.name,
-    //             chapters: parsedChapters,
-    //             books:parsedBooks
-    // });
-
-
 }
 
 
-async function main() {
-    const url = 'https://unibs.coursecatalogue.cineca.it/insegnamenti/2023/8293_120752_344/2019/8293/118?coorte=2023&schemaid=2753'
+async function processSyllabusBooks(syllabus: CourseInfo) {
+    const systemPromptBooks = `You are a helpful studybuddy for university students, you are given in input a string with the content of the course, 
+      you should infer list of the books used in this universitycourse, create a json a and fill the schema as you believe it is useful for the student, the 
+      output should follow this schema:{books: [{ name: string, authors: string[], notes: string[]}`
 
-    const syllabus = await scrapeSyllabus(url)    
+    let response = await openai.chat.completions.create({
+        model: "gpt-4o-mini",  // Make sure to specify the correct model
+        messages: [
+            { role: "system", content: systemPromptBooks },
+            { role: "user", content: syllabus.books },
+        ],
+        response_format: { type: "json_object" },
+    });
+
+    const parsedBooks = response.choices[0].message.content
+
+    return parsedBooks
+
+}
+
+async function getsyllabus(url: string) {
+
+    const syllabus = await scrapeSyllabus(url)
 
 
+    const processedChapters: processedChapter = await processSyllabusChapters(syllabus)
 
-    
+    console.log(processedChapters);
 
-    const processedChapters = await processSyllabusChapters(syllabus)
 
-    console.log(processedChapters?.chapters);
+    // Execute the regex against the syllabus name
+    const id = syllabus.name.match(/\[(\w+)\]/)?.[1] || '';
+    const name = syllabus.name.split(']').slice(1).join(']').replace(/[^\w\s]/g, ' ').trim();
+    ;
+
+    const uniname = 'unibs'
+
 
     const exam = {
-        name: syllabus.name,
-        chapters: processedChapters?.chapters
+        name: name,
+        id: uniname + id,
+        icon: processedChapters?.icon,
+        color: processedChapters?.color,
+        chapters: processedChapters?.chapters,
+        links: [{ "name": "syllabus", "url": url },],
+        postIts: [{
+            "color": "#FFEB3B",
+            "content": syllabus.books
+        },
+        {
+            "color": "#FFEB3B",
+            "content": syllabus.examDetails
+        }]
+
     }
 
     const fileName = `${syllabus.name.replace(/\s+/g, '_')}.json`;
@@ -171,7 +191,7 @@ async function main() {
     // Convert the exam object to a stringified JSON
     const jsonString = JSON.stringify(exam, null, 2);
 
-    fs.writeFile(fileName, jsonString, (err) => {
+    fs.writeFile('data/syllabus/' + fileName, jsonString, (err) => {
         if (err) {
             console.error("Error writing file:", err);
         } else {
@@ -179,9 +199,40 @@ async function main() {
         }
     });
 
-    
 
-    
+
+
 }
 
-main()
+const url = 'https://unibs.coursecatalogue.cineca.it/insegnamenti/2023/8249_122421_8338/2022/8251/81?coorte=2023&schemaid=2493'
+const url1 = 'https://unibs.coursecatalogue.cineca.it/insegnamenti/2023/8108_115535_145/2022/8110/81?coorte=2022&schemaid=2808'
+
+getsyllabus(url1)
+
+
+
+async function main() {
+    const path = '/Users/alessiogandelli/dev/studybuddy/uni-scraper/data/unibs_esami.json'
+
+
+    interface Course { name: string, id: string, uni: string, exams: [] }
+
+    const jsonData = JSON.parse(fs.readFileSync(path, 'utf8'));
+    const ingemec: Course = jsonData.find((obj: Course) => obj.id === 'unibs05742'); // Converts obj.id to string for comparison
+    for (const exam of ingemec.exams) {
+
+        try{
+            await getsyllabus(exam['url'])
+        }catch(e){
+            console.log(exam['title'] + 'is broken');
+            console.log(exam['url']);
+            
+            
+        }
+
+    }
+
+}
+
+//main()
+
