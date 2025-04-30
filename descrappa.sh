9a54a3236149a9affa6c3336249dc310b2b83518
Nicola Toscan <nicolatoscan99@outlook.it>
2025-04-30 21:53:45 +0200

    Descrap script

---
+++ descrappa.sh
@@ -0,0 +1,20 @@
+echo "Staring"
+echo "Did you remember to set up config.ts?"
+echo ""
+
+echo "Running 10"
+ts-node src/descrapper/10-courses.ts
+
+echo "Running 11"
+ts-node src/descrapper/11-courses-parse.ts
+
+echo "Running 20"
+ts-node src/descrapper/20-course-exams.ts
+
+echo "Running 21"
+ts-node src/descrapper/21-courses-exam-parser.ts
+
+echo "Running 30"
+ts-node src/descrapper/30-exams.ts
+
+echo "Done"
