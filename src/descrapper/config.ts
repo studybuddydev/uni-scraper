85ee694d7c1581f71ad539e1f7afeea406bee75f
Nicola Toscan <nicolatoscan99@outlook.it>
2025-05-06 22:55:06 +0200

    Exams by year in course

--- src/descrapper/config.ts
+++ src/descrapper/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   scripingName: 'triennaliUNIBS',
-  unicode: 'unibs',
-  url: '/corsi/2024?gruppo=1617109934164',
+  unicode: 'unitn',
+  url: '/corsi/2024?gruppo=1647269677464',
 }
